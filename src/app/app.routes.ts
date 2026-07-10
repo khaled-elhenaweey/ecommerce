6a48daa8a376b107ad7a9129d400e46672b93aba
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-07-10 09:18:10 +0300

    feat(layout): add header, footer, and shell components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Shell } from '../layout/shell/shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Shell,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('../features/home/home').then((m) => m.Home),
+      },
+    ],
+  },
+];
