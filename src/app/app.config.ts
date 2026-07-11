f5d20d988f2b4ac00df77d022c5493aae980a393
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-07-11 16:20:17 +0300

    fix(core): re-enable zone.js change detection for imperative subscribe pattern

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
